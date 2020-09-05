db9665d868871e368c0bdb4ca88c86c82d5fc227
dimamar1 <dimamar1@ya.ru>
2020-09-05 14:15:58 +0300

    решение

---
+++ src/WordsStatistic/Data/WordFrequency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsStatistic.Data
+{
+    /// <summary>
+    /// Описатель частоты слова
+    /// </summary>
+    public class WordFrequency
+    {
+        private int _count = 1;
+
+        public int Count
+        {
+            get { return _count; }
+        }
+    }
+}
---
+++ src/WordsStatistic/MainVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsStatistic
+{
+    public class MainVM:INotifyPropertyChanged
+    {
+
+        public bool IsStarted { get; set; }
+
+        public double Progress { get; set; }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
